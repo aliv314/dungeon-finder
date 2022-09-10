d55056dafcd3d6dbdb7b3ffb955f4063e2cef221
sisenberg219 <samisenberg@gmail.com>
2022-09-09 23:35:53 -0400

    MongoDB Implemented

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import Flask
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = ""
+app.config["MONGO_URI"] = "mongodb+srv://tinky:<password>@tinkydb.9hvmnek.mongodb.net/?retryWrites=true&w=majority"
 
+#setup mongodb 
+mongodb_client = PyMongo(app)
+db = mongodb_client.db
 
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
-
 
 if __name__ == '__main__':
     app.run(debug=True)
